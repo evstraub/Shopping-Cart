8ce6780da2e4012035821aa26841a398f530f7eb
Ethan Straub <evstraub@gmail.com>
2023-03-27 11:56:45 -0400

    first commit to new repo

---
+++ src/components/context/Context.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useReducer } from "react";
+import { faker} from "@faker-js/faker";
+import { cartReducer, productReducer } from "./Reducer";
+
+const Cart = createContext();
+// faker.seed(99)
+
+const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    // eslint-disable-next-line no-undef
+    image:faker.image.business(640,480,true),
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+
+  console.log(productState);
+
+  return (
+    <Cart.Provider value={{ state, dispatch, productState, productDispatch }}>
+      {children}
+    </Cart.Provider>
+  );
+};
+
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
